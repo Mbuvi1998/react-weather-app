cd81b82d6be26600b7bbddec983e864e686fcb49
Mbuvi1998 <mbuvidan67@gmail.com>
2024-10-17 09:14:27 +0300

    created a TopButton Component

---
+++ src/components/TopButtons.jsx
@@ -0,0 +1,26 @@
+
+
+const TopButtons = () => {
+  const cities = [
+    { id: 1, name: 'London' },
+    { id: 2, name: 'Sydney' },
+    { id: 3, name: 'Tokyo' },
+    { id: 4, name: 'Paris' },
+    { id: 5, name: 'Toronto' },
+  ];
+
+  return (
+    <div className="flex items-center justify-around my-6">
+      {cities.map((city) => (
+        <button
+          key={city.id}  // Add a key prop for each item in the map
+          className="text-lg font-medium hover:bg-gray-700/20 px-3 py-2 rounded-md transition ease-in"
+        >
+          {city.name}
+        </button>
+      ))}
+    </div>
+  );
+};
+
+export default TopButtons;
