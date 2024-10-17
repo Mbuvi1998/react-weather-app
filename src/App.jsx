1025b40ddd19bba474e8a3d9ac33f4e6e47242b1
Mbuvi1998 <mbuvidan67@gmail.com>
2024-10-17 23:16:38 +0300

    Created WeatherService

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,43 @@ import Inputs from "./components/Inputs";
 import TimeAndLocation from "./components/TimeAndLocation";
 import TempAndDetails from "./components/TempAndDetails";
 import Forecast from "./components/Forecast";
+import getFormattedWeatherData from "./Services/weatherService";
+import { useEffect, useState } from "react";
 
 const App = () => {
+
+
+  const [query, setQuery] = useState({q: 'london'});
+  const [units, setUnits] = useState('metric');
+  const [weather, setWeather] = useState(null);
+
+  const getWeather = async () => {
+    
+    await getFormattedWeatherData({q: 'ahmedabad'}).then( data =>{
+      setWeather(data);
+    });
+    console.log(data)
+  }
+
+  useEffect(() => { 
+    getWeather()
+}, [query, units])
+  
+
+  getWeather();
   return (
     <div className="mx-auto max-w-screen-lg mt-4 py-5 px-32 bg-gradient-to-br shadow-xl shadow-gray-400 from-cyan-600 to-blue-700">
       <TopButtons />
       <Inputs />
 
-      <TimeAndLocation />
-      <TempAndDetails />
+      { weather &&(
+        <>
+      <TimeAndLocation weather={weather}/>
+      <TempAndDetails weather={weather}/>
       <Forecast />
       <Forecast />
+        </>
+      )}
     </div>
   )
 }
--- src/components/TimeAndLocation.jsx
+++ src/components/TimeAndLocation.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 
-const TimeAndLocation = () => {
+const TimeAndLocation = ({weather: {formattedLocalTime, name, country},
+
+}) => {
   return (
     <div>
       <div className='flex items-center justify-center my-6'>
-        <p className='text-xl font-extralight '>
-            Thursday, 17 Oct 2024 | Local time: 10:12 AM
-        </p>
+        <p className='text-xl font-extralight '>{formattedLocalTime}</p>
       </div>
       <div className='flex items-center justify-center my-3'>
-        <p className='text-3xl font-medium'>Berlin, DE</p>
+        <p className='text-3xl font-medium'>{'${name}, ${country}'}</p>
       </div>
     </div>
   )
